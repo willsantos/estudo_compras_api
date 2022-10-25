37bb9c53dca2c785e9497ee958438cad964e99c3
Wilson Santos <contato@wilsonsantos.net>
2022-10-25 15:17:34 -0300

    :memo: Add Db Context

---
+++ Wilson.Compras.Api/src/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wilson.Compras.Api.Models;
+
+namespace Wilson.Compras.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    
+    public DbSet<Item> Items { get; set; }
+    public DbSet<ShopList> Lists { get; set; }
+    public DbSet<ListItem> Listitems { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=127.0.0.1,1439;Database=compras_wilson_santos;User=SA;Password=wd%!r$H7Ez@yuPz*sx*3wUgBcwv;"
+        );
+    }
+
+}
