ecb8c37ad8779a8f46006bb9ab53948c14066d73
Wilson Santos <contato@wilsonsantos.net>
2022-10-25 15:18:02 -0300

    :sparkles: Add Base Repository

---
+++ Wilson.Compras.Api/src/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Wilson.Compras.Api.Interfaces;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<T> FindAsync(int id);
+    Task<int> CountAsync();
+    Task<List<T>> ListAsync();
+    Task AddAsync(T item);
+    Task EditAsync(T item);
+    Task RemoveAsync(T item);
+}
---
+++ Wilson.Compras.Api/src/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wilson.Compras.Api.Data;
+using Wilson.Compras.Api.Interfaces;
+
+namespace Wilson.Compras.Api.Repositories;
+
+public abstract class BaseRepository<T>: IBaseRepository<T> where T:class
+{
+    private readonly AppDbContext _context;
+    
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<T> FindAsync(int id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<int> CountAsync()
+    {
+        return await _context.Set<T>().CountAsync();
+    }
+
+    public async Task<List<T>> ListAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task AddAsync(T item)
+    {
+        await _context.Set<T>().AddAsync(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(T item)
+    {
+        _context.Entry(item).State = EntityState.Modified;
+        //_context.Update(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(T item)
+    {
+        _context.Set<T>().Remove(item);
+        await _context.SaveChangesAsync();
+    }
+}
